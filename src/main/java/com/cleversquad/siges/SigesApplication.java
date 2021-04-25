2ab658fbcf5d460473d0c6a6c1ca0b540864c356
omarkourmou <kourmou.omar@gmail.com>
2021-04-25 22:48:16 +0000

    {omarkourmou}{initialisation back siges}

---
+++ src/main/java/com/cleversquad/siges/SigesApplication.java
@@ -0,0 +1,13 @@
+package com.cleversquad.siges;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cleversquad/siges/SigesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cleversquad.siges;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
